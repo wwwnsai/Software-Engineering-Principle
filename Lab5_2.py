a6440e8c05312d0ed2eca2ac349809c7c83f80b4
shoviper <65011304@kmitl.ac.th>
2023-12-28 13:45:11 +0700

    a

--- Lab5_2.py
+++ Lab5_2.py
@@ -34,6 +34,12 @@ class Taxi( Transportation ):
     def find_cost( self ):
       return (40 * self.distance)
 
+class Train( Transportation ):
+   def __init__(self, start, end, distance):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost(self):
+      return (5 * self.distance)
    
 # main program
 
