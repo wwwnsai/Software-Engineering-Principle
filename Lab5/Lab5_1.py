a0c9fa1eb239e74a4498f30a0feec9dd25469217
wwwnsai <65011588@kmitl.ac.th>
2023-12-28 13:32:18 +0700

    Simple_drawing_window2

--- Lab5/Lab5_1.py
+++ Lab5/Lab5_1.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class simple_drawing_window(QWidget):
+class simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Github Drawing")
@@ -27,10 +27,38 @@ class simple_drawing_window(QWidget):
 
         p.drawPixmap(QRect(200,100,320,320), self.rabbit)
 
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Github Drawing2")
+        self.rabbit = QPixmap("Lab5/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPie(50, 100, 100, 100, 0, 45 * 16)
+
+        p.setPen(QColor(0, 127, 0))
+        p.drawLine(100, 150, 55, 200)
+        p.drawLine(100, 150, 145, 200)
+
+        p.setPen(QColor(255, 0, 0))
+        p.setBrush(QColor(255, 0, 0))
+        p.drawPie(30, 200, 50, 50, 0, 360 * 16)
+        p.drawPie(120, 200, 50, 50, 0, 360 * 16)
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
 def main():
     app = QApplication(sys.argv)
-    w = simple_drawing_window()
-    w.show()
+    w1 = simple_drawing_window1()
+    w2 = Simple_drawing_window2()
+    w1.show()
+    w2.show()
 
     return app.exec()
 
